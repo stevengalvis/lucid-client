839694c5647292065cd97e67146b84b0ea957c95
stevengalvis <steven.galvis@gmail.com>
2018-11-20 22:14:51 -0500

    added components and actions and reducers folders

---
+++ src/components/Control/Control.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Control(props) {
+  return <div className="control" />;
+}
---
+++ src/components/Structure/Structure.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Structure(props) {
+  return <div className="structure" />;
+}
